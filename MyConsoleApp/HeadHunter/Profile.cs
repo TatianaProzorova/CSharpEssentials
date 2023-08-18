5c75b69d2747b2fe48705502595455705d210b95
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-18 19:54:57 +0300

    HeadHunter minor update

--- MyConsoleApp/HeadHunter/Profile.cs
+++ MyConsoleApp/HeadHunter/Profile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyConsoleApp
 {
@@ -19,7 +20,22 @@ namespace MyConsoleApp
         public void Describe()
         {
             Console.WriteLine($"{ProfessionName}, {GetInfo()}");
-        }            
+        }
+
+        // code in Main()
+        //    List<Profile> profiles = new List<Profile>
+        //    {
+        //        new Vacancy("C# разработчик", 100000),
+        //        new Vacancy("Python разработчик", 90000),
+        //        new Vacancy("C++ разработчик", 110000),
+        //        new Resume("C# разработчик", 4),
+        //        new Resume("C++ разработчик", 1),
+        //    };
+
+        //    foreach (var profile in profiles)
+        //    {
+        //        profile.Describe();
+        //    }
     }
 }
 
--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -10,19 +10,7 @@ namespace MyConsoleApp
     {
         static void Main()
         {
-            List<Profile> profiles = new List<Profile>
-            {
-                new Vacancy("C# разработчик", 100000),
-                new Vacancy("Python разработчик", 90000),
-                new Vacancy("C++ разработчик", 110000),
-                new Resume("C# разработчик", 4),
-                new Resume("C++ разработчик", 1),
-            };
 
-            foreach (var profile in profiles)
-            {
-                profile.Describe();
-            }
         }
     }
 }
