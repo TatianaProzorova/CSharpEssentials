f92679c482626778181b2a0bc126d52445f58d12
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-18 17:57:11 +0300

    Implementation of class Profile

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -7,13 +7,49 @@ using System.Threading.Tasks;
 
 namespace MyConsoleApp
 {
+    public class Profile
+    {
+        public string ProfessionName { get; set; }
+
+        public Profile(string professionName)
+        {
+            ProfessionName = professionName;
+        }
+
+        protected virtual string GetInfo()
+        {
+            return String.Empty;
+        }
+
+        public void Describe(string professionName)
+        {
+            string result;
+            result = GetInfo();
+            Console.WriteLine($"{professionName}, {result}");
+        }            
+    }
 
 
     class Program
     {
         static void Main()
         {
+            List<Profile> profiles = new List<Profile>
+            {
+                new Vacancy("C# разработчик", 100000),
+                new Vacancy("Python разработчик", 90000),
+                new Vacancy("C++ разработчик", 110000),
+                new Resume("C# разработчик", 4),
+                new Resume("C++ разработчик", 1),
+            };
 
+            foreach (var profile in profiles)
+            {
+                profile.Describe();
+            }
         }
     }
 }
+
+
+
