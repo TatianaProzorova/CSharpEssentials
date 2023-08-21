0a6aa6ffb576a56e0c69060d04533aba47e3543d
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-21 19:46:36 +0300

    Struct Money

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Security.Policy;
 
 namespace MyConsoleApp
 {
-
-    public class Program
+    public partial class Program
     {
-        public class A
-        {
-            static A()
-            {
-                Console.WriteLine("Да! Я понял как создавать статический конструктор!");
-            }
-        }
+
         static void Main()
         {
 
         }
     }
 }
-
-
-
---
+++ MyConsoleApp/Struct_Money/Money.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    public partial class Program
+    {
+        public struct Money
+        {
+            private decimal ruble;
+            private decimal kopeck;
+            public decimal Ruble 
+            {
+                get { return ruble; }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException("Rubles must not be < 0");
+                    }
+                    else ruble = value;
+                }
+            }
+
+            public decimal Kopeck
+            {
+                get { return kopeck; }
+                set
+                {
+                    if (value < 0 || value > 99) 
+                    {
+                        throw new Exception("Kopecks are out of range [0;99]");
+                    }
+                    else kopeck = value;
+                }
+            }
+
+            public Money(decimal ruble, decimal kopeck)
+            {                
+                this.ruble = ruble;
+                this.kopeck = kopeck;
+                Ruble = ruble; 
+                Kopeck = kopeck; 
+            }
+
+            public override string ToString()
+            {
+                return $"{ruble}, {string.Format("{0:00}", kopeck)}";
+            }
+
+            // code from Main()
+            //Money money1 = new Money(10, 50);
+            //Console.WriteLine(money1); // 10, 50
+
+            //money1.Ruble = 4;
+            //money1.Kopeck = 5;
+            //Console.WriteLine(money1); // 4, 05
+
+            //money1.Kopeck = 100; // ошибка
+        }
+    }
+}
