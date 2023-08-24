b0fb5a63d1ada0f252a86b7d6f78cd4495f33aca
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-24 08:24:23 +0300

    Delegate. Anonymous

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -7,7 +7,12 @@ namespace MyConsoleApp
     {
         static void Main()
         {
-            Print(new List<int> { 1, 2, 3, 4, 5 }, IsEven);
+            Func<int, bool> predicate = delegate (int number)
+            {
+                return Math.Pow(number, 2) % 10 == 9;
+            };
+
+            Print(new List<int> { 1, 2, 3, 4, 5, 7, 13, 27 }, predicate);
         }
 
         static void Print(List<int> items, Func<int, bool> predicate)
@@ -19,11 +24,6 @@ namespace MyConsoleApp
                     Console.Write(item + " ");
                 }
             }
-        }
-
-        private static bool IsEven(int item)
-        {
-            return item % 2 == 0;
         }
     }
 }
