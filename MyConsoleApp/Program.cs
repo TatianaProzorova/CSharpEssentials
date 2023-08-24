6203bc5b3590819da2a3f7ecce5697a31f7d6211
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-24 14:50:16 +0300

    Enum: Calculator

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,30 +6,35 @@ namespace MyConsoleApp
 {
     class Program
     {
-        public enum Months
+        public enum Operations
         {
-            January,
-            February,
-            March,
-            April,
-            May,
-            June,
-            July,
-            August,
-            September,
-            October,
-            November,
-            December
+            Add,
+            Subtract,
+            Multiply,
+            Divide
         }
 
-        public static int GetDay(Months month)
+        public static double Calc(double item1, double item2, Operations operation)
         {
-            return DateTime.DaysInMonth(2023, (int)month + 1); // enum index starts from zero
+            if (operation == Operations.Divide && item2 == 0)
+            {
+                throw new ArgumentException("Divide by zero exception");
+            }
+
+            switch (operation)
+            {
+                case Operations.Add: return item1 + item2;  
+                case Operations.Subtract: return item1 - item2; 
+                case Operations.Multiply: return item1 * item2; 
+                case Operations.Divide: return item1 / item2;                  
+                default: return 0;
+            }
         }
 
         static void Main()
         {
-                Console.WriteLine($"{GetDay(Months.February)}");
+            Console.WriteLine(Calc(2.5, 2, Operations.Add));
+            Console.WriteLine(Calc(2.5, 2, Operations.Subtract));
         }
     }
 }
