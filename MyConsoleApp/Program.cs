27a3e8438e91914e9571c663cb04bd2a2d693dd3
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-24 21:45:38 +0300

    Exception: throw new Exception

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,16 +6,28 @@ namespace MyConsoleApp
 {
     class Program
     {
+        public class Phone
+        {
+            public string Name { get; }
+            public decimal Cost { get; }
+            public Phone(string name, decimal cost)
+            {
+                if (name != null)
+                    Name = name;
+                else throw new ArgumentNullException("Имя не может быть равно null!");
+
+                if (name != String.Empty && name.Length >= 2 && name.Length <= 150)
+                    Name = name;
+                else throw new ArgumentException("Имя должно содержать от 2 до 50 символов!");
+
+                if (cost > 0)
+                    Cost = cost;
+                else throw new ArgumentException("Значение цены должно быть положительным!");
+            }
+        }
+        
         static void Main()
         {
-            try
-            {
-                WriteToFile();
-            }
-            catch (NullReferenceException e)
-            {
-                Console.WriteLine(e.Message);
-            }
 
             Console.WriteLine("Дальнейшеее выполнение программы");
         }
