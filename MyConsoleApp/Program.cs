07eb15dbbf57212fb2bb155cdcdd4d479d1a9e96
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-24 20:46:59 +0300

    Exceptions. Try catch

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,40 +6,18 @@ namespace MyConsoleApp
 {
     class Program
     {
-        public enum Colors
-        {
-            Red,
-            Black,
-            Yellow,
-            White
-        }
-
-        public enum Languages
-        {
-            English,
-            Russian
-        }
-
-        public static string ColorTranslate(Colors color, Languages language)
-        {
-            switch (color, language)
-            {
-                case (Colors.Red, Languages.Russian): return "Красный";
-                case (Colors.Black, Languages.Russian): return "Чёрный";
-                case (Colors.Yellow, Languages.Russian): return "Жёлтый";
-                case (Colors.White, Languages.Russian): return "Белый";
-                case (Colors.Red, Languages.English): return "Red";
-                case (Colors.Black, Languages.English): return "Black";
-                case (Colors.Yellow, Languages.English): return "Yellow";
-                case (Colors.White, Languages.English): return "White";
-                default: return String.Empty;
-            }
-        }
-
         static void Main()
         {
-            Console.WriteLine(ColorTranslate(Colors.Red, Languages.Russian)); // Красный
-            Console.WriteLine(ColorTranslate(Colors.Red, Languages.English)); // Red
+            try
+            {
+                WriteToFile();
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Дальнейшеее выполнение программы");
         }
     }
 }
