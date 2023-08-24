f9ec1553a85aebd3073240332be049c4fe0ab720
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-24 06:22:13 +0300

    Delegate. Action<int> action

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,77 +1,26 @@
 ﻿using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Xml.Linq;
-using System.Security.Policy;
 using System;
-using System.Diagnostics;
 
 namespace MyConsoleApp
 {
-    public class User
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-
-        public User(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name}, {Age}";
-        }
-    }
-
     class Program
     {
-        delegate void Rule(User user);
-
-        private static void LongNameRule(User user)
-        {
-            if (user.Name.Length > 10)
-            {
-                user.Name = user.Name.Substring(0, 10);
-            }
-        }
-
-        private static void IncreaseAge(User user)
-        {
-            user.Age++;
-        }
-
-        private static List<User> TransformUsersByRule(List<User> users, Rule rule)
-        {
-            foreach (User user in users) 
-            {
-                rule(user);
-            }
-            return users;
-        }
-
         static void Main()
         {
-            List<User> users = new List<User>
+            Print(new List<int> { 1, 2, 3, 4, 5 }, Twice);
+        }
+
+        static void Print(List<int> items, Action<int> action)
+        {
+            foreach (var item in items)
             {
-                new User("namenamename", 25),
-                new User("1234567890", 34),
-                new User("123456", 14),
-            };
-
-            List<User> transformedUsers = TransformUsersByRule(users, LongNameRule);
-
-            transformedUsers = TransformUsersByRule(users, IncreaseAge);
-
-            foreach (var user in transformedUsers)
-            {
-                Console.WriteLine(user);
+                action(item);
             }
+        }
+
+        static void Twice(int item)
+        {
+            Console.Write($"{item * 2} ");
         }
     }
 }
