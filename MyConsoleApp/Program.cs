920664ccd2f42fbff4eff972b1eeb87870afb249
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-26 17:10:41 +0300

    Events. Register new user

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections.Generic;
 using System;
 using static MyConsoleApp.Program;
+using System.Configuration;
 
 namespace MyConsoleApp
 {
     class Program
     {
-        public class MyClass
+        public class User
         {
-            public void Method1()
-            {
+            public string Name;
+            public string Email;
+            public string Password;
 
+            public User(string name, string email, string password)
+            {
+                Name = name;
+                Email = email;
+                Password = password;
             }
         }
-        
+
+        public class UserManager
+        {
+            public List<User> Users {get;}
+            
+            public UserManager()
+            {
+                Users = new List<User>();
+            }  
+            
+            public void Register(User user)
+            {
+                Users.Add(user);
+            }
+        }
+
         static void Main()
         {
-            try
-            {
-                MyClass action = new MyClass();
-                action.Method1();
-            }
-            catch (MyException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
         }
     }
 }
