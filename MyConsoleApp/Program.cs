54785d0cb078c27efee9e88972fa2edbd0532e71
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-09-04 19:03:39 +0300

    LINQ

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,15 +1,24 @@
 ﻿using CSharpEssentials;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace CSharpEssentials
 {
 
+
     class Program
     {
         static void Main(string[] args)
         {
+            static List<string> GetLongNames(List<string> names)
+            {
+                var longNames = names // источник
+                                    .Where(name => name.Length > 3) // запрос
+                                    .Select(name => name);
 
+                return longNames.ToList();
+            }
         }
     }
 }
